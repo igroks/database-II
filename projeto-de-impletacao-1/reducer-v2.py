170a1b5cc5903c48c7e5414ac3f7c38caa863ffc
Igor <igor.carvalho@icomp.ufam.edu.br>
2022-11-14 10:09:38 -0400

    fix: add complete reducer

--- projeto-de-impletacao-1/reducer-v2.py
+++ projeto-de-impletacao-1/reducer-v2.py
@@ -9,3 +9,18 @@ word = None
 for line in sys.stdin:
     line = line.strip()
     line=line.lower()
+    word, count = line.split('\t', 1)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print ('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+if current_word == word:
+    print( '%s\t%s' % (current_word, current_count))
